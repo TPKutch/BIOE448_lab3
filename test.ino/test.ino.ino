1743118ab6246dd96f639b6550e5f3f772d1553c
Thomas Kutcher <thomaskut123@gmail.com>
2024-01-30 15:29:44 -0600

    new code

--- test.ino/test.ino.ino
+++ test.ino/test.ino.ino
@@ -1,10 +1,14 @@
 #include <LiquidCrystal.h>
-void setup() {
-  // put your setup code here, to run once:
 
+void setup() {
+  LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+  lcd.begin(16, 2); //Initiate the LCD in a 16x2 configuration
+  lcd.print("My name is:");
+  lcd.setCursor(0, 1);
+  lcd.print("Thomas");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  delay(50);
 }
